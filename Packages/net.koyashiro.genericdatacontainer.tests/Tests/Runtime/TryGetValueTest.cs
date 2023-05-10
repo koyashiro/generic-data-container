aa910d25d436d3c37ba6f2e4939bde4740533d81
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-05-10 20:35:28 +0900

    fix: support user-defined types (#2)

--- Packages/net.koyashiro.genericdatacontainer.tests/Tests/Runtime/TryGetValueTest.cs
+++ Packages/net.koyashiro.genericdatacontainer.tests/Tests/Runtime/TryGetValueTest.cs
@@ -7,6 +7,8 @@ namespace Koyashiro.GenericDataContainer.Tests
     [AddComponentMenu("")]
     public class TryGetValueTest : UdonSharpBehaviour
     {
+        public TestUserDefinedClass testUserDefinedClass;
+
         public void Start()
         {
             var list = DataList<int>.New(new int[] { 100, 200, 300, 400 });
@@ -26,6 +28,12 @@ namespace Koyashiro.GenericDataContainer.Tests
 
             Assert.False(list.TryGetValue(4, out output));
             Assert.Equal(default(int), output);
+
+            var list2 = DataList<TestUserDefinedClass>.New();
+            list2.Add(testUserDefinedClass);
+            TestUserDefinedClass output2;
+            Assert.True(list2.TryGetValue(0, out output2));
+            Assert.Equal("Hello, world!", output2.field, this);
         }
     }
 }
